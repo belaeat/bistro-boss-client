bdce434d27361cf1c571eaf36b8806d873ae925d
belaeat <omyhossain@gmail.com>
2023-06-09 00:24:50 +0600

    reviews data loaded from server

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -3,7 +3,7 @@ import FoodCard from "../../../components/FoodCard/FoodCard";
 
 const OrderTab = ({ items }) => {
     return (
-        <div className='grid md:grid-cols-3 gap-10'>
+        <div className='grid md:grid-cols-3 gap-10 my-12'>
             {
                 items.map(item => <FoodCard
                     key={item._id}
